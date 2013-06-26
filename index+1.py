ca326ba40ae9894a0373cbbd0ae3710716171dfb
Antonio Viggiano <antonio.viggiano@usp.br>
2013-06-25 21:54:13 -0300

    index+1.py now works. TODO make itto receive the filename as input parameter

--- index+1.py
+++ index+1.py
@@ -8,28 +8,33 @@ from array import *
 import codecs
 
 def do_the_change(all_arroba):
-		i_0 = 0
-		hasChanged = False
-		all_arroba2 = []
+	i_0 = 0
+	hasChanged = False
+	all_arroba2 = []
 
-		for i in all_arroba:
-			if re.findall(r'@\d+', i, re.U):
-				if not hasChanged:
-					i_0 = re.sub(r'@(\d+)', r'\1', i)
-					print i
-					all_arroba2.append(i)
-				else:
-					i_0 = str(int(i_0)+1)
-					i = re.sub(r'@(\d+)', '@'+str(int(i_0)+1), i)
-					print i
-					all_arroba2.append(i)
-			else:
-				i = re.sub(r'@(change)', '@'+str(int(i_0)+1), i)
-				print i
+	for i in all_arroba:
+		if re.findall(r'@\d+', i, re.U):
+			if not hasChanged:
+				i_0 = re.sub(r'@(\d+)', r'\1', i)
 				all_arroba2.append(i)
-				hasChanged = True
+			else:
+				i_0 = str(int(i_0)+1)
+				i = re.sub(r'@(\d+)', '@'+str(int(i_0)+1), i)
+				all_arroba2.append(i)
+		else:
+			i = re.sub(r'@(change)', '@'+str(int(i_0)+1), i)
+			all_arroba2.append(i)
+			hasChanged = True
 
-		return all_arroba2
+	return all_arroba2
+
+def do_the_plus_1 (to_change):
+	ans = []
+	for i in to_change:
+		i = re.sub(r'@(\d+)', r'\1', i)
+		ans.append('@'+str(int(i)+1))
+	return ans
+
 
 print "Reading input latex..."
 
@@ -44,12 +49,13 @@ output_file = open(latex_output_filename, 'w');
 
 print "Refactoring latex..."
 index_db = 1
+changeFound = False
 
 for i in range(len(latex_lines)):
 	plus_1 = re.findall(r'@change', latex_lines[i], re.U)
-	if (len (plus_1) == 0):
+	if (len (plus_1) == 0 and not changeFound):
 		output_file.write(latex_lines[i])
-	else:
+	elif (not changeFound):
 		to_change = re.findall(r'@\d+|@change', latex_lines[i], re.U)
 		all_arroba = re.findall(r'@\d+|@change', latex_lines[i], re.U)
 		all_arroba = do_the_change(all_arroba)
@@ -58,12 +64,22 @@ for i in range(len(latex_lines)):
 		change_to = all_arroba
 
 		ii = 0
-		print 'veja o bug: '
-		print 'a troca deveria ser de uma vez so!!!!!'
+		to_change.reverse()
+		change_to.reverse()
 		for i in to_change:
-			print i
-			print change_to[ii]
-			print to_be_written + '\n'
+			#print to_be_written + '\n'
+			to_be_written = re.sub(i, change_to[ii], to_be_written)
+			ii += 1
+		output_file.write(to_be_written.encode('utf8'))
+		changeFound = True
+	elif (changeFound):
+		to_change = re.findall(r'@\d+', latex_lines[i], re.U)
+		to_change.reverse()
+		change_to = do_the_plus_1 (to_change)
+
+		ii = 0
+		to_be_written = latex_lines[i].decode('utf8')
+		for i in to_change:
 			to_be_written = re.sub(i, change_to[ii], to_be_written)
 			ii += 1
 		output_file.write(to_be_written.encode('utf8'))
